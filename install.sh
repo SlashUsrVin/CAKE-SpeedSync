e6dcfc6d42a69539504b7552707c2882c8fd5597
Vin <m.vin.321@outlook.com>
2025-03-14 11:15:27 +0800

    Initial version

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 
 GIT_REP_URL="https://raw.githubusercontent.com/mvin321/MERLIN-dyn-tc-cake/main"
 TGT_DIR="/jffs/scripts/dyn-tc-cake"
+GIT_TOKEN="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"
 
 mkdir -p "$TGT_DIR"
 
-curl -fsSL "$GIT_REP_URL/.ashrc" -o "$TGT_DIR/.ashrc"
-curl -fsSL "$GIT_REP_URL/.profile" -o "$TGT_DIR/.profile"
-curl -fsSL "$GIT_REP_URL/dyn-tc-cake.sh" -o "$TGT_DIR/dyn-tc-cake.sh"
-curl -fsSL "$GIT_REP_URL/services-start" -o "$TGT_DIR/services-start"
+curl -fsSL -H "Authorization: token $GIT_TOKEN" "$GIT_REP_URL/.ashrc" -o "$TGT_DIR/.ashrc"
+curl -fsSL -H "Authorization: token $GIT_TOKEN" "$GIT_REP_URL/.profile" -o "$TGT_DIR/.profile"
+curl -fsSL -H "Authorization: token $GIT_TOKEN" "$GIT_REP_URL/dyn-tc-cake.sh" -o "$TGT_DIR/dyn-tc-cake.sh"
+curl -fsSL -H "Authorization: token $GIT_TOKEN" "$GIT_REP_URL/services-start" -o "$TGT_DIR/services-start"
