a07673d838f60f5ebeee841d49edab01f931e04d
Vin <m.vin.321@outlook.com>
2025-03-14 11:00:49 +0800

    Initial version

---
+++ install.sh
@@ -0,0 +1,4 @@
+curl -fsSL "https://github.com/mvin321/MERLIN-dyn-tc-cake/blob/main/.ashrc" -o /jffs/scripts/dyn-tc-cake/.ashrc
+curl -fsSL "https://github.com/mvin321/MERLIN-dyn-tc-cake/blob/main/.profile" -o /jffs/scripts/dyn-tc-cake/.profile
+curl -fsSL "https://github.com/mvin321/MERLIN-dyn-tc-cake/blob/main/dyn-tc-cake.sh" -o /jffs/scripts/dyn-tc-cake/yn-tc-cake.sh
+curl -fsSL "https://github.com/mvin321/MERLIN-dyn-tc-cake/blob/main/services-start" -o /jffs/scripts/dyn-tc-cake/services-start
