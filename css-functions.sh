65834a9abb7be1bcf846110272faa5d9bea553c8
Vin <m.vin.321@outlook.com>
2025-03-20 17:37:09 +0800

    updated css_pkt_qos to convert dscp tag value from hex to class name before saving to qosports config file

--- css-functions.sh
+++ css-functions.sh
@@ -83,8 +83,8 @@ css_pkt_qos () {
    #Record ports for re-applying iptables on reboot
    #iptables -t mangle -S | awk '/POSTROUTING/ && /DSCP/ && 0x2e' | grep -oE 'dport [0-9]+(\:[0-9]+)?' | grep -oE '[0-9]+(\:[0-9]+)?' > /jffs/scripts/cake-speedsync/qosports
    #iptables -t mangle -S | awk '/POSTROUTING/ && /DSCP/ {print $8, $NF, $4}' > /jffs/scripts/cake-speedsync/qosports
-   iptables -t mangle -S | awk '/POSTROUTING/ && /DSCP/ {print $8, $NF, $4}' > /jffs/scripts/cake-speedsync/qosports
-   cat /jffs/scripts/cake-speedsync/qosports | while read -r xport xhextag xproto; do
+   rm -f /jffs/scripts/cake-speedsync/qosports
+   while read -r xport xhextag xproto; do
       case "$xhextag" in
          0x2e) xtag="EF";;
          0x28) xtag="CS5";;
@@ -93,6 +93,6 @@ css_pkt_qos () {
          *) xtag="CS0";;
       esac
       echo "$xport $xtag $xproto" >> /jffs/scripts/cake-speedsync/qosports
-   done
+   done < <(iptables -t mangle -S | awk '/POSTROUTING/ && /DSCP/ {print $8, $NF, $4}')
    
 }
